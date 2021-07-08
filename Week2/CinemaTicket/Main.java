99ccafe1edc19e66585c971ba16ae1b109163f21
Hatice SÖNMEZ <htc.sonmez@hotmail.com>
2021-07-08 22:19:20 +0300

    comment lines added

--- Week2/CinemaTicket/Main.java
+++ Week2/CinemaTicket/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        List<Movie> movieList = new ArrayList<Movie>();
+        List<Movie> movieList = new ArrayList<Movie>();  // Movie türünden liste oluşturularak filmler ekleniyor.
 
         movieList.add(new Movie(1, "The Butterfly Effect ", "Science Fiction", 20.00));
         movieList.add(new Movie(2, "Titanic", "Drama", 23.00));
@@ -14,7 +14,7 @@ public class Main {
         movieList.add(new Movie(4, "Halloween", "Horror", 25.00));
         movieList.add(new Movie(5, "The Ring", "Horror", 27.00));
 
-        DataInput data = new DataInput();
+        DataInput data = new DataInput();  // Müşteri girişi yapılabilmesi için DataInput sınıfından nesne oluşturularak getData() fonksiyonu çağrılıyor.
         data.getData(movieList);
     }
 
