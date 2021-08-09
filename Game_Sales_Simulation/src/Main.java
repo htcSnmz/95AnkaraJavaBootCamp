5b369d4ef12b7c453a2007b3fb5789f5ab27e923
Hatice SÖNMEZ <htcsonmez428@gmail.com>
2021-08-09 23:53:13 +0300

    Update Main.java

--- Game_Sales_Simulation/src/Main.java
+++ Game_Sales_Simulation/src/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("---------------------------");
+        System.out.println("\n---------------------------");
         System.out.println("OYUN KANALINA HOŞGELDİNİZ");
         System.out.println("---------------------------");
 
